fbd96f73f9a0a77b88e155e65067da04cc59853a
Chris Su <chrisssu19@gmail.com>
2026-05-21 12:59:28 -0400

    fix: update gitignore for .o, change signal to proper arith errors

--- rt/c0vc.c
+++ rt/c0vc.c
@@ -11,22 +11,30 @@ void __c0vc_abort(const char *msg) {
   exit(1);
 }
 
+__attribute__((noreturn))
+void __c0vc_arith_error(const char *msg) {
+  fprintf(stderr, "%s\n", msg);
+  fflush(stderr);
+  raise(SIGFPE);
+  exit(1);
+}
+
 int __c0vc_checked_div(int lhs, int rhs) {
   if (rhs == 0) {
-    __c0vc_abort("division by zero");
+    __c0vc_arith_error("division by zero");
   }
   if (lhs == INT_MIN && rhs == -1) {
-    __c0vc_abort("division overflow");
+    __c0vc_arith_error("division overflow");
   }
   return lhs / rhs;
 }
 
 int __c0vc_checked_mod(int lhs, int rhs) {
   if (rhs == 0) {
-    __c0vc_abort("modulo by zero");
+    __c0vc_arith_error("modulo by zero");
   }
   if (lhs == INT_MIN && rhs == -1) {
-    __c0vc_abort("modulo overflow");
+    __c0vc_arith_error("modulo overflow");
   }
   return lhs % rhs;
 }
